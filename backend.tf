e43a8db74b2d09a784703d91384483b2a0b4e03a
Akshay Telang <akshayit22@gmail.com>
2025-02-18 15:46:35 +0530

    aws configure

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket         = "akshay-s3-bucket-terraform"
     key            = "terraform_learning/terraform.tfstate"
     region         = "us-east-1"
-    # dynamodb_table = "akshay-terraform-state-locking"
+    dynamodb_table = "akshay-terraform-state-locking"
   }
 }
